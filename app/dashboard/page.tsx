0ebd50b7fc0367bf10a99cc3f7e2d34479f880db
ziad abdalla <ziadabdalla846@gmail.com>
2026-01-02 15:55:39 +0200

    convert layout to  server components

--- app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import DashboardShell from "../_components/DashboardShell";
-
-export default function DashboardPage() {
-  return (
-    <DashboardShell>
-      <h1 className="text-2xl font-bold">Dashboard</h1>
-    </DashboardShell>
-  );
-}
